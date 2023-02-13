dfbba6eb927d8acb87e411a4437a8daf3f6af851
guifroes1984 <guilhermefroes26@gmail.com>
2023-02-13 20:09:07 -0300

    Colocando as dependencias, e criando a primeira classe.

---
+++ jsf-e-primefaces/src/main/java/com/lifedev/erp/model/RamoAtividade.java
@@ -0,0 +1,14 @@
+package com.lifedev.erp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table()
+public class RamoAtividade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
