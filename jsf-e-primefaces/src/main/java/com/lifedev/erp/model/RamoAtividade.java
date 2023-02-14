055cc9f4dcb5f264f966f00baaadc8fb261beb7c
guifroes1984 <guilhermefroes26@gmail.com>
2023-02-13 23:42:32 -0300

    Configurando o persistence.xml

--- jsf-e-primefaces/src/main/java/com/lifedev/erp/model/RamoAtividade.java
+++ jsf-e-primefaces/src/main/java/com/lifedev/erp/model/RamoAtividade.java
@@ -2,6 +2,7 @@ package com.lifedev.erp.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +18,8 @@ public class RamoAtividade implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@Column(nullable = false, length = 80)
 	private String descricao;
 
 	public Long getId() {
